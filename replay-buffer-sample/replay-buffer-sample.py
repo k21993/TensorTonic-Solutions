a6b6230c5fee26fefe0f7c9ae758da106cf53da2
Karthik Suresh <karthiksuresha2012@gmail.com>
2026-07-13 00:41:18 -0700

    Solve replay-buffer-sample

---
+++ replay-buffer-sample/replay-buffer-sample.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def replay_buffer_sample(buffer, batch_size, seed):
+    """
+    Sample a batch of transitions from the replay buffer.
+    """
+    # Write code here
+    rng = np.random.RandomState(seed)
+    idx = rng.choice(len(buffer), size=batch_size, replace=False)
+    buffer = np.array(buffer)
+    return buffer[idx]
